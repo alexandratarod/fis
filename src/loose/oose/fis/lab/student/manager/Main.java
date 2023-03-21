364da4bf14fb37861fe7ee9b7228ad47f477c057
alexandratarod <alexandratarod@yahoo.ro>
2023-03-21 16:40:46 +0200

    modif

--- src/loose/oose/fis/lab/student/manager/Main.java
+++ src/loose/oose/fis/lab/student/manager/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
-        primaryStage.setTitle("FIS");
+        return primaryStage;
         primaryStage.setScene(new Scene(root, 600, 400));
         //return primaryStage;
     }
